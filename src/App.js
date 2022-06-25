f1167020aa045889534e1e7128e989f619ceef3b
khalidWid <khalid.idouarab@gmail.com>
2022-06-25 18:05:42 +0200

    name change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-//import AboutPage from "./pages/AboutPage";
+import AboutPage from "./pages/AboutPage";
 import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
